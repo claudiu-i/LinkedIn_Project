5d8bf6ccbeffe348c7c3359706b9df7ca13d488c
Ethan Coskay <ethancoskay@gmail.com>
2025-03-13 20:19:27 -0400

    Added minor visual change

--- client/src/app.js
+++ client/src/app.js
@@ -6,45 +6,45 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Room from './pages/Room';
 import CreateRoom from './pages/CreateRoom';
 import Profile from './pages/Profile';
-import LinkedInPage  from './pages/LinkedInPage';
+import LinkedInPage from './pages/LinkedInPage';
 import { CallProvider } from './components/CallContext';
 import CallModal from './components/CallModal';
 import CallDashboard from './components/CallDashboard';
-
+//
 function App() {
   return (
     <Router>
-    <CallProvider>
+      <CallProvider>
         {/* The modal will be available throughout the app */}
         <CallModal top={70} />
-      <div className="app-container">
-        <Routes>
-          {/* Public routes */}
-          <Route path="/" element={<LinkedInPage />} />
-          {/* Protected routes */}
-          <Route path="/home" element={<CallDashboard />} />
-          <Route
-            path="/room/:roomId"
-            element={
-              <Room />
-            }
-          />
-          <Route
-            path="/create-room"
-            element={
-              <CreateRoom />
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <Profile />
-            }
-          />
-          {/* Redirect any unknown routes */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
+        <div className="app-container">
+          <Routes>
+            {/* Public routes */}
+            <Route path="/" element={<LinkedInPage />} />
+            {/* Protected routes */}
+            <Route path="/home" element={<CallDashboard />} />
+            <Route
+              path="/room/:roomId"
+              element={
+                <Room />
+              }
+            />
+            <Route
+              path="/create-room"
+              element={
+                <CreateRoom />
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <Profile />
+              }
+            />
+            {/* Redirect any unknown routes */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
       </CallProvider>
     </Router>
   );
