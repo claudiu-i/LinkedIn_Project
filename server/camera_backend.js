624afa61b2aea7c03d3e4d50f44c15cf7ee16c0c
Ethan Coskay <ethancoskay@gmail.com>
2025-03-19 09:49:03 -0400

    Added constants for backend

---
+++ server/camera_backend.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000", // React Frontend
+    methods: ["GET", "POST"]
+  }
+});
